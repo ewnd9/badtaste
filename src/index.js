aaf8230743c058e42593823c62ef3ef334683e06
ewnd9 <ewndnine@gmail.com>
2015-09-20 05:42:18 +0300

    better credentials handling, new screenshot

--- src/index.js
+++ src/index.js
@@ -18,15 +18,23 @@ import storage from './storage';
 
 import Promise from 'bluebird';
 
-var token = {
-  name: 'token',
+let authUrl = 'https://oauth.vk.com/authorize?client_id=5075122&scope=audio,offline&redirect_uri=https://oauth.vk.com/blank.html&display=page&v=5.23&response_type=token';
+let responseExample = 'https://oauth.vk.com/blank.html#access_token=<85 symbols>&expires_in=0&user_id=<user_id>';
+
+let token = {
+  name: 'url',
   type: 'input',
-  hint: 'https://oauth.vk.com/authorize?client_id=5075122&scope=audio,offline&redirect_uri=https://oauth.vk.com/blank.html&display=page&v=5.23&response_type=token',
+  hint: `Open "${authUrl}" in browser.\nCopy paste new url here.\nIt should look "${responseExample}"`,
   env: 'VK_TOKEN'
 };
 
+let tokenRegex = /.+access_token=([a-z0-9]+)&.+/g;
+
 inquirerCredentials('.badtaste-npm-credentials', [token]).then(function(credentials) {
-  vk.setToken(credentials.token);
+  var match = tokenRegex.exec(credentials.url);
+  
+  var token = match[1];
+  vk.setToken(token);
 
   let { screen, leftPane, rightPane } = tui();
 
