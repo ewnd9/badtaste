f689dbe8369ab6451aa2ad120c2832afa219e6ab
ewnd9 <ewndnine@gmail.com>
2015-11-08 19:58:36 +0300

    add update-notifier package, fixes #11

--- cli.js
+++ cli.js
@@ -4,5 +4,9 @@ var intel = require('intel');
 intel.addHandler(new intel.handlers.File(process.env.HOME + '/.badtaste-npm-info.log'));
 global.Logger = intel;
 
+var updateNotifier = require('update-notifier');
+var pkg = require('./package.json');
+updateNotifier({pkg: pkg}).notify();
+
 require('babel/register')({ only: __dirname + '/src' });
 require('./src/index');
